608de0c81a528cc1415469b8e2c351b31ddbb0a8
Jahaziel <jahzerahj@gmail.com>
2025-01-13 15:19:38 +0100

    added pagination to fetch url

--- index.js
+++ index.js
@@ -11,30 +11,27 @@ const nextButton = document.querySelector('[data-js="button-next"]');
 const pagination = document.querySelector('[data-js="pagination"]');
 
 // States
-const maxPage = 1;
+const maxPage = 42;
 const page = 1;
 const searchQuery = "";
 
 async function fetchCharacters() {
-  const response = await fetch("https://rickandmortyapi.com/api/character");
+  const response = await fetch(
+    `https://rickandmortyapi.com/api/character/?page=${page}`
+  );
   const data = await response.json();
   //data.results is an array of objects with character data
   return data.results;
 }
 
-
 function renderCards(characters) {
-  console.log(characters)
+  cardContainer.innerHTML = "";
+  console.log(characters);
   characters.forEach((character) => {
     const card = CharacterCard(character);
     cardContainer.append(card);
   });
-};
-
-
-
-
-
+}
 
 // ----------------------------------------------------------------------------------
 
